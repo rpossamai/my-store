4935343ed3d35dd88e77fccfbbf1939586d1d25a
Ricardo Possamai <ricardopossamai@gmail.com>
2022-09-13 15:48:17 -0400

    config connection to postgres DB

--- services/user.service.js
+++ services/user.service.js
@@ -1,14 +1,23 @@
 const boom = require('@hapi/boom');
 
+const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
+
 class UserService {
-  constructor() {}
+  constructor() {
+    this.users = [];
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err)); 
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    return [];
+    const query = 'SELECT * FROM task';
+    const rta= await this.pool.query(query);
+    return rta.rows;
   }
 
   async findOne(id) {
